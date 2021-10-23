049e788226a06537c48b3f96c14f55a755d2b568
Botchromosome <91787098+Botchromosome@users.noreply.github.com>
2021-10-23 09:53:16 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const os = process
 global.DeveloperMode = 'false' //true Or false
-global.owner = ['6281220670449', '6282298891772'] || os.env["6281220670449"]// Put your number here
+global.owner = ['6281804832728', '6285943175444'] || os.env["6281804832728"]// Put your number here
 global.linkGC = ['https://chat.whatsapp.com/FBy8oRq3umC7SbjIFdeMgu:']
-global.mods = ['6281220670449', '6282298891772'] // Want some help?
-global.prems = ['6281220670449', '6282298891772'] // Premium user has unlimited limit
+global.mods = ['6281804832728', '6285943175444'] // Want some help?
+global.prems = ['6281804832728', '62825943175444'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -21,7 +21,7 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'Sticker By Sherlynn Botz'
+global.packname = 'Sticker By ChM Botz'
 global.author = 'JAN LUPA DONATE'
 
 global.multiplier = 69 // The higher, The harder levelup
